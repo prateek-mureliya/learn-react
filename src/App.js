44dba249044d8c43cf50a17f09dff47ca4393162
Prateek Kumar Mureliya <prateekkumar1393@gmail.com>
2021-08-02 01:10:53 +0530

    tailwindcss installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-import './App.css'
+import './style.css'
 
 const App = () => {
     return (
         <>
-            <h2>hello world</h2>
+            <ul class="grid grid-cols-10 h-7">
+                <li class="bg-pink-50"></li>
+                <li class="bg-pink-100"></li>
+                <li class="bg-pink-200"></li>
+                <li class="bg-pink-300"></li>
+                <li class="bg-pink-400"></li>
+                <li class="bg-pink-500"></li>
+                <li class="bg-pink-600"></li>
+                <li class="bg-pink-700"></li>
+                <li class="bg-pink-800"></li>
+                <li class="bg-pink-900"></li>
+            </ul>
         </>
     )
 }
