0ffb7d72234ffb9e064a9a9344893357ea6a4d23
Prateek Kumar Mureliya <prateekkumar1393@gmail.com>
2021-08-02 15:30:53 +0530

    created confirmation popup box

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './style.css'
+import Button from "./components/Button";
+import Popup from "./components/Popup";
 
 const App = () => {
+    const [showModal, setShowModal] = useState(false)
+
+    const showModalHandler = () => {
+        setShowModal(true);
+    }
+
+    const closeModalHandler = () => {
+        setShowModal(false);
+    }
+
     return (
         <>
-            <ul class="grid grid-cols-10 h-7">
-                <li class="bg-pink-50"></li>
-                <li class="bg-pink-100"></li>
-                <li class="bg-pink-200"></li>
-                <li class="bg-pink-300"></li>
-                <li class="bg-pink-400"></li>
-                <li class="bg-pink-500"></li>
-                <li class="bg-pink-600"></li>
-                <li class="bg-pink-700"></li>
-                <li class="bg-pink-800"></li>
-                <li class="bg-pink-900"></li>
-            </ul>
+            <div className="flex justify-center mt-16">
+                <Button
+                    className="bg-yellow-600 hover:bg-yellow-700 text-white border-transparent"
+                    onClick={showModalHandler}
+                >Open model</Button>
+            </div>
+
+            <Popup
+                heading="Deactivate account"
+                text="Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone."
+                showModal={showModal}
+                closeModal={closeModalHandler}
+                onConfirm={()=> alert("You Clicked Deactivate")}
+                />   
         </>
     )
 }
